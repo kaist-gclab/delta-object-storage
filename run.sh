df2bc7e6e3f44c4ee7ccea3fd3c2efbfca1c3098
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-24 01:19:45 +0900

    저장 장치 증설에 맞게 설정을 변경합니다

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ docker run -d -p 9000:9000 \
 -e "MINIO_SECRET_KEY=SECRET_KEY_HERE" \
 --name delta-object-storage \
 --restart=unless-stopped \
--v /var/delta/object-storage:/data \
-minio/minio server /data
+-v /mnt/data1/delta/object-storage:/data1 \
+minio/minio server /data1
