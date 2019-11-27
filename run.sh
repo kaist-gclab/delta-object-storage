23a340bded45b70bb1c22ca9b782d37c5aa14b9d
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-11-28 03:18:15 +0900

    환경 변수가 제대로 지정되지 않았을 때 경고하도록 합니다

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
+: "${DELTA_OBJECT_STORAGE_ACCESS_KEY?DELTA_OBJECT_STORAGE_ACCESS_KEY}"
+: "${DELTA_OBJECT_STORAGE_SECRET_KEY?DELTA_OBJECT_STORAGE_SECRET_KEY}"
+
 docker run -d -p 9000:9000 \
--e "MINIO_ACCESS_KEY=ACCESS_KEY_HERE" \
--e "MINIO_SECRET_KEY=SECRET_KEY_HERE" \
+-e "MINIO_ACCESS_KEY=$DELTA_MINIO_ACCESS_KEY" \
+-e "MINIO_SECRET_KEY=$DELTA_MINIO_SECRET_KEY" \
 --name delta-object-storage \
 --restart=unless-stopped \
 -v /mnt/data1/delta/object-storage:/data1 \
