f8b2f5bd558121f62fb6868ed1751c601441d796
Hyeonguk Ryu <ryu@hyeonguk.com>
2019-12-04 16:51:58 +0900

    데이터 디렉터리를 환경 변수로 지정하도록 합니다

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 : "${DELTA_OBJECT_STORAGE_ACCESS_KEY?DELTA_OBJECT_STORAGE_ACCESS_KEY}"
 : "${DELTA_OBJECT_STORAGE_SECRET_KEY?DELTA_OBJECT_STORAGE_SECRET_KEY}"
+: "${DELTA_OBJECT_STORAGE_DATA_DIRECTORY?DELTA_OBJECT_STORAGE_DATA_DIRECTORY}"
 
 docker run -d -p 9000:9000 \
 -e "MINIO_ACCESS_KEY=$DELTA_MINIO_ACCESS_KEY" \
 -e "MINIO_SECRET_KEY=$DELTA_MINIO_SECRET_KEY" \
 --name delta-object-storage \
 --restart=unless-stopped \
--v /mnt/data1/delta/object-storage:/data1 \
+-v "$DELTA_OBJECT_STORAGE_DATA_DIRECTORY:/data1" \
 minio/minio server /data1
